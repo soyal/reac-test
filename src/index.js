44a4b77dadeae499489b332777983c491423ed84
soyal <xiongxiao9958@gmail.com>
2017-11-05 09:17:43 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import Test from './styled-component'
-// import Counter from './mobx/count/count'
-// import counterStore from './mobx/count/store'
-
-// const store = new counterStore()
-import FsEditor from '@fs/fs-editor'
-import '@fs/fs-editor/dist/fs-editor.css'
-
-render(<div>
-  <FsEditor onImageInsert={(file, base64, insertImage) => {
-    // ...
-  }}></FsEditor>
-</div>, document.querySelector('#root'))
+import Test from 'test'
+const a = Test
+render(<div>{a}</div>, document.querySelector('#root'))
 
